17f827e1ab1b89ca1d2c48978ec88c0632eca5de
Антон <sierra_93@mail.ru>
2022-03-25 21:23:53 +0300

    Начал добавлять таблицы.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Garant.Platform.Database
+Garant.Platform.Database - это универсальная кросс-платформенная библиотека Liquibase для миграций БД.
---
+++ pg_update_test.ps1
@@ -0,0 +1,17 @@
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="schemasonly" update
+
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="typesonly"   update
+
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="tablesonly"   update
+
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="viewssonly"   update
+
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="functionsonly"   update
+
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="proceduresonly"   update
+
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="specialonly"   update
+
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="specialonly"   update
+
+liquibase --defaultsFile=pgsql.leoka_elementarydb_test.properties --changeLogFile=pgsql.leoka_elementarydb_test.xml --contexts="Common"   update
