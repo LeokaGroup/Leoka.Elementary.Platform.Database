4ed8b12f319f383fdc6b7023e08d54a8e2401c84
Oyarsa <sierra_93@mail.ru>
2022-05-07 23:44:03 +0300

    Добавил новые столбцы.

--- Profile/pgsql.leoka_elementarydb_test/Tables/[Profile].[ProfileMenuItems]/[Profile].[ProfileMenuItems]_CreateTable.sql
+++ Profile/pgsql.leoka_elementarydb_test/Tables/[Profile].[ProfileMenuItems]/[Profile].[ProfileMenuItems]_CreateTable.sql
@@ -1,7 +1,14 @@
-CREATE TABLE IF NOT EXISTS "Profile"."ProfileMenuItems" (
-	"ProfileMenuId" serial PRIMARY KEY,
-	"ProfileItemUrl" TEXT NULL,
+CREATE TABLE "Profile"."ProfileMenuItems" (
+	"ProfileMenuId" serial4 NOT NULL,
+	"ProfileItemUrl" text NULL,
 	"ProfileItemTitle" varchar(200) NOT NULL,
 	"ProfileItemSysName" varchar(200) NOT NULL,
-	"Position" int NOT NULL DEFAULT 0
+	"Position" int4 NOT NULL DEFAULT 0,
+	"RoleId" int NOT NULL DEFAULT 1,
+	"IsSelectItem" BOOLEAN NOT NULL DEFAULT FALSE,
+	"MenuType" int NOT NULL DEFAULT 0,
+	"IconUrl" TEXT NULL,
+	"IsVisibleBalance" BOOLEAN NOT NULL DEFAULT FALSE,
+	"IsDropdown" BOOLEAN NOT NULL DEFAULT FALSE,
+	CONSTRAINT "ProfileMenuItems_pkey" PRIMARY KEY ("ProfileMenuId")
 );
