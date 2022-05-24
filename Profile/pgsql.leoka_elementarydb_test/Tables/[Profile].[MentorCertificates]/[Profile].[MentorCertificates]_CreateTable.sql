095b69061ac24065de68aa9683cb0c7e5601a4fe
Oyarsa <sierra_93@mail.ru>
2022-05-24 17:54:50 +0300

    Добавил userid в таблицу информации о профиле пользователя.

--- Profile/pgsql.leoka_elementarydb_test/Tables/[Profile].[MentorCertificates]/[Profile].[MentorCertificates]_CreateTable.sql
+++ Profile/pgsql.leoka_elementarydb_test/Tables/[Profile].[MentorCertificates]/[Profile].[MentorCertificates]_CreateTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "Profile"."MentorCertificates" (
 	"CertificateId" BIGSERIAL,
-	"CertificateUrl" TEXT NOT NULL,
+	"CertificateName" TEXT NOT NULL,
 	"UserId" BIGINT NOT NULL,
 	CONSTRAINT "PK_MentorCertificatesCertificateId" PRIMARY KEY ("CertificateId")
 );
