575d72a470d5339af29491a8d1bd20b7f2f39ed9
Oyarsa <sierra_93@mail.ru>
2022-05-29 00:25:06 +0300

    Удалил лишние столбцы из таблицы предметов преподавателя.

--- Profile/pgsql.leoka_elementarydb_test/Tables/[Profile].[MentorProfileItems]/[Profile].[MentorProfileItems]_CreateTable.sql
+++ Profile/pgsql.leoka_elementarydb_test/Tables/[Profile].[MentorProfileItems]/[Profile].[MentorProfileItems]_CreateTable.sql
@@ -1,7 +1,5 @@
 CREATE TABLE IF NOT EXISTS "Profile"."MentorProfileItems" (
 	"ItemId" BIGSERIAL,
-	"ItemName" VARCHAR(200) NOT NULL,
-	"ItemSysName" VARCHAR(200) NOT NULL,
 	"Position" INT4 NOT NULL DEFAULT 0,
 	"UserId" BIGINT NOT NULL,
 	CONSTRAINT "PK_MentorProfileItemsItemId" PRIMARY KEY ("ItemId")
